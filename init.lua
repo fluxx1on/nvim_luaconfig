59ef42f1108c0783beaf3664413109f7882db9b2
fluxx1on <nick.kaliga@ya.ru>
2023-07-19 16:10:49 +0300

    Init

--- init.lua
+++ init.lua
@@ -1,5 +1,25 @@
 require "core"
 
+vim.opt.swapfile = false
+
+-- Настройка расширенного буфера обмена
+vim.opt.clipboard:append("unnamedplus")
+
+-- Настройка колонки цвета
+vim.opt.colorcolumn = "79"
+
+
+-- hotkeys
+vim.api.nvim_set_keymap('n', ',<space>', ':nohlsearch<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'S', '<Nop>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-z>', 'ZZ', { noremap = true })
+vim.api.nvim_set_keymap('n', 'y', '$y', { noremap = true })
+
+vim.api.nvim_set_keymap('i', '<c-s>', '<esc>:w<cr>i', { noremap = true })
+vim.api.nvim_set_keymap('i', '<c-u>', '<esc>:u<cr>i', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-z>', '<Esc>', { noremap = true })
+
 local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
 if custom_init_path then
